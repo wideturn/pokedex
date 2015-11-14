62aedd78311badc808f8247ef11802ccc40d11ba
Sam Atwell <sold14@gmail.com>
2015-11-14 11:29:44 -0500

    fixed keyboard not going away

--- pokedex-by-core/ViewController.swift
+++ pokedex-by-core/ViewController.swift
@@ -26,6 +26,7 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         collection.delegate = self
         collection.dataSource = self
         searchBar.delegate = self
+        searchBar.returnKeyType = UIReturnKeyType.Done
         
         parsePokemonCSV()
         initAudio()
@@ -118,9 +119,15 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         }
     }
     
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        view.endEditing(true)
+    }
+    
     func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
         if searchBar.text == nil || searchBar.text == "" {
             inSearchode = false
+            view.endEditing(true)
+            collection.reloadData()
         } else {
             inSearchode = true
             let lower = searchBar.text!.lowercaseString
